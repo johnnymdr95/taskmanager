c4741a66f6d154d5670c6b860a661b946a8a121e
johnnymdr95 <109240526+johnnymdr95@users.noreply.github.com>
2024-12-01 23:45:14 +0530

    Update index.js

--- taskmanager_backend/db/index.js
+++ taskmanager_backend/db/index.js
@@ -1,11 +1,14 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'postgres', // replace with your PostgreSQL username
-  host: 'localhost',
-  database: 'postgres',
-  password: 'root', // replace with your password
+  user: 'taskmanager_fnha_user', 
+  host: 'dpg-ct69i2bqf0us738dg0s0-a',
+  database: 'taskmanager_fnha',
+  password: 'nN9w67XM5zi1lqoQcmH2tbgwGMvNbccW', 
   port: 5432,
+  ssl: {
+    rejectUnauthorized: false, 
+  },
 });
 
 pool.query('SELECT NOW()', (err, res) => {
